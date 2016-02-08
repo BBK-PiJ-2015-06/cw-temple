9be9479ad395752a30d6501a6b3634c5c3e0ccf5
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-02-08 11:57:37 +0000

    Possibly useful fields added to Escape class. Priority queue used to hold a queue of PathStatus objects. Intention is to prioritise the objects based upon the total gold accumulated, and then the poll() method will  be used to eliminate those that have a timeTakenValue that is too great.

--- src/student/EscapeAlgorithm/Escape.java
+++ src/student/EscapeAlgorithm/Escape.java
@@ -1,13 +1,22 @@
 package student.EscapeAlgorithm;
 
 import game.EscapeState;
+import game.Node;
+import student.PriorityQueue;
+import student.PriorityQueueImpl;
 
 public class Escape {
 
     private EscapeState currentState;
+    private Node source;
+    private Node destination;
+    private PriorityQueue<PathStatus> possiblePaths;
 
     public Escape(EscapeState state) {
-        this.currentState = state;
+        currentState = state;
+        source = currentState.getCurrentNode();
+        destination = currentState.getExit();
+        possiblePaths = new PriorityQueueImpl<>();
     }
 
     public void findExit() {}
