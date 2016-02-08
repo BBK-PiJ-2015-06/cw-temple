90d597e4cdab063781d60f9964c16bdcdd24d51c
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-02-08 12:06:36 +0000

    Structure of Escape class created. Proposed algorithm written in findExit method.

--- src/student/EscapeAlgorithm/Escape.java
+++ src/student/EscapeAlgorithm/Escape.java
@@ -10,14 +10,37 @@ public class Escape {
     private EscapeState currentState;
     private Node source;
     private Node destination;
+    private int timeRemaining;
     private PriorityQueue<PathStatus> possiblePaths;
 
     public Escape(EscapeState state) {
         currentState = state;
         source = currentState.getCurrentNode();
         destination = currentState.getExit();
+        timeRemaining = currentState.getTimeRemaining();
         possiblePaths = new PriorityQueueImpl<>();
     }
 
-    public void findExit() {}
+    public void findExit() {
+        MazeGraph graph = buildGraph();
+        populatePaths(graph);
+        PathStatus pathToTake = decideOptimalPath();
+        escapeMaze(pathToTake);
+    }
+
+    private MazeGraph buildGraph() {
+        return null;
+    }
+
+    private void populatePaths(MazeGraph graph) {
+
+    }
+
+    private PathStatus decideOptimalPath() {
+        return null;
+    }
+
+    private void escapeMaze(PathStatus path) {
+        
+    }
 }
