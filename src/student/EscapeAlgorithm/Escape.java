b38de90586ed9f183732f4d68f37c935de091375
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-02-10 10:27:16 +0000

    New method updateGoldRank added to class Escape to rank location nodes based upon their distance from the current node and the amount of gold they contain.

--- src/student/EscapeAlgorithm/Escape.java
+++ src/student/EscapeAlgorithm/Escape.java
@@ -2,7 +2,9 @@ package student.EscapeAlgorithm;
 
 import game.EscapeState;
 import game.Node;
+import student.PriorityQueue;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Stack;
 import java.util.stream.Collectors;
@@ -11,10 +13,13 @@ public class Escape {
 
     private EscapeState currentState;
     private Collection<Node> goldNodes;
+    private Collection<Node> outOfReach;
+    private PriorityQueue<Node> goldRank;
 
     public Escape(EscapeState state) {
         currentState = state;
         goldNodes = currentState.getVertices();
+        outOfReach = new ArrayList<>();
         updateGoldNodes();
     }
 
@@ -35,7 +40,25 @@ public class Escape {
     }
 
     private void updateGoldNodes() {
-        goldNodes = goldNodes.stream().filter(n -> n.getTile().getGold() > 0).collect(Collectors.toList());
+        goldNodes = goldNodes.stream().filter(n ->
+                n.getTile().getGold() > 0 && !outOfReach.contains(n)).collect(Collectors.toList());
+    }
+
+    private void updateGoldRank() {
+        for(Node n : goldNodes) {
+            ShortestPathFinder toGold = new ShortestPathFinder(currentState, currentState.getCurrentNode(), n);
+            ShortestPathFinder toExit = new ShortestPathFinder(currentState, n, currentState.getExit());
+
+            int distanceToGold = toGold.getDistance();
+            int distanceToExit = toExit.getDistance();
+            int total = distanceToExit + distanceToGold;
+
+            if(total < currentState.getTimeRemaining()) {
+                goldRank.add(n, distanceToGold - n.getTile().getGold());
+            } else {
+                outOfReach.add(n);
+            }
+        }
     }
 }
 
