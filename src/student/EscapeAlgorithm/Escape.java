40ee22d226ea9158b7b667532f1abb9134afa2d4
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-02-10 09:45:51 +0000

    New private field created which will keep a record of all nodes with gold on them that remain in the maze.

--- src/student/EscapeAlgorithm/Escape.java
+++ src/student/EscapeAlgorithm/Escape.java
@@ -3,14 +3,19 @@ package student.EscapeAlgorithm;
 import game.EscapeState;
 import game.Node;
 
+import java.util.Collection;
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 public class Escape {
 
     private EscapeState currentState;
+    private Collection<Node> goldNodes;
 
     public Escape(EscapeState state) {
         currentState = state;
+        Collection<Node> vertices = currentState.getVertices();
+        goldNodes = vertices.stream().filter(n -> n.getTile().getGold() > 0).collect(Collectors.toList());
     }
 
     public void escapeMaze() {
