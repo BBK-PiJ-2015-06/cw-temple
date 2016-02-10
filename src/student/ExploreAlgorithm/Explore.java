70ec45222f604730441f43faae62b360f76eaa9f
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-02-10 13:49:24 +0000

    New Explore class created

---
+++ src/student/ExploreAlgorithm/Explore.java
@@ -0,0 +1,12 @@
+package student.ExploreAlgorithm;
+
+import game.ExplorationState;
+
+public class Explore {
+
+    private ExplorationState currentState;
+
+    public Explore(ExplorationState state) {
+        currentState = state;
+    }
+}
