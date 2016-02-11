5358a967f9788588b20416a1b5c5993837e4f5fd
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-02-11 11:01:06 +0000

    New HashMaps created to represent graph of NodeStatus objects discovered so far, and these will ultimately be partitioned into visited and not visited.

--- src/student/Explore.java
+++ src/student/Explore.java
@@ -4,14 +4,23 @@ import game.ExplorationState;
 import game.NodeStatus;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Explore {
 
     private ExplorationState state;
+    private Map<Long, GraphNode> wholeGraph;
+    private Map<Long, GraphNode> visitedGraph;
+    private Map<Long, GraphNode> notVisitedGraph;
+
 
     public Explore(ExplorationState state) {
         this.state = state;
+        wholeGraph = new HashMap<>();
+        visitedGraph = new HashMap<>();
+        notVisitedGraph = new HashMap<>();
     }
 
     public void findOrb() {
