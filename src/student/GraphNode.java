ad6e606afaecb8d70769e95ab8145626931f4531
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-02-11 12:02:19 +0000

    Neighbours field added to GraphNode class.

--- src/student/GraphNode.java
+++ src/student/GraphNode.java
@@ -2,13 +2,18 @@ package student;
 
 import game.NodeStatus;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GraphNode {
 
     private NodeStatus node;
     private boolean visited = false;
+    private List<GraphNode> neighbours;
 
     public GraphNode(NodeStatus node) {
         this.node = node;
+        neighbours = new ArrayList<>();
     }
 
     public NodeStatus getNode() {
@@ -21,5 +26,13 @@ public class GraphNode {
 
     public void setVisited(boolean b) {
         visited = b;
+    }
+
+    public List<GraphNode> getNeighbours() {
+        return neighbours;
+    }
+
+    public void setNeighbours(List<GraphNode> neighbours) {
+        this.neighbours = neighbours;
     }
 }
