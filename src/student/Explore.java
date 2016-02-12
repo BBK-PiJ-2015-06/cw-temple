466414b384612a9368a6cca116919fbe0f44c08d
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-02-12 10:42:09 +0000

    Will attempt to use Stack to keep track of current path in latest optimisation attempt

--- src/student/Explore.java
+++ src/student/Explore.java
@@ -1,5 +1,22 @@
 package student;
 
+import game.ExplorationState;
+
+import java.util.Stack;
+
 public class Explore {
+
+    private ExplorationState state;
+    private Stack<Long> currentPath;
+
+    public Explore(ExplorationState state) {
+        this.state = state;
+    }
+
+    public void findOrb() {
+        while(state.getDistanceToTarget() != 0) {
+            
+        }
+    }
 
 }
