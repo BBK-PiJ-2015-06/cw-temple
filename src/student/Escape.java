91eb4e727fd6b06459d4eb62d3ecd8aad0cfe4f5
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-02-23 09:02:11 +0000

    Reverted back to original escape algorithm

---
+++ src/student/Escape.java
@@ -0,0 +1,80 @@
+package student;
+
+import game.EscapeState;
+import game.Node;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Escape {
+
+    private EscapeState currentState;
+    private Collection<Node> goldNodes;
+    private Map<Long, Node> outOfReach;
+    private PriorityQueue<Node> goldRank;
+
+    private static final double GOLD_FACTOR = 1;
+    private static final double DIST_FACTOR = 1.9;
+
+    public Escape(EscapeState state) {
+        currentState = state;
+        goldNodes = currentState.getVertices();
+        outOfReach = new HashMap<>();
+        goldRank = new PriorityQueueImpl<>();
+
+        if(currentState.getCurrentNode().getTile().getGold() > 0) {
+            currentState.pickUpGold();
+        }
+        updateGoldNodes();
+        updateGoldRank();
+    }
+
+    public void escapeMaze() {
+        while(currentState.getCurrentNode().getId() != currentState.getExit().getId()) {
+            if(goldRank.size() == 0) {
+                ShortestPathFinder toExit = new ShortestPathFinder(currentState, currentState.getCurrentNode(), currentState.getExit());
+                followPath(toExit.getPath());
+            } else {
+                ShortestPathFinder toGold = new ShortestPathFinder(currentState, currentState.getCurrentNode(), goldRank.poll());
+                followPath(toGold.getPath());
+                updateGoldNodes();
+                updateGoldRank();
+            }
+        }
+    }
+
+    private void followPath(Stack<DijVertex> path) {
+        path.pop();
+        while (!path.empty()) {
+            Node nextNode = path.pop().getNode();
+            currentState.moveTo(nextNode);
+            if(nextNode.getTile().getGold() > 0) {
+                currentState.pickUpGold();
+            }
+        }
+    }
+
+    private void updateGoldNodes() {
+        goldNodes = goldNodes.stream().filter(n ->
+                n.getTile().getGold() > 0 && !outOfReach.containsKey(n.getId())).collect(Collectors.toList());
+    }
+
+    private void updateGoldRank() {
+        goldRank = new PriorityQueueImpl<>();
+        for(Node n : goldNodes) {
+            ShortestPathFinder toGold = new ShortestPathFinder(currentState, currentState.getCurrentNode(), n);
+            ShortestPathFinder toExit = new ShortestPathFinder(currentState, n, currentState.getExit());
+
+            int distanceToGold = toGold.getDistance();
+            int distanceToExit = toExit.getDistance();
+            int total = distanceToExit + distanceToGold;
+
+            if(total < currentState.getTimeRemaining()) {
+                goldRank.add(n, (distanceToGold * DIST_FACTOR) - (n.getTile().getGold() * GOLD_FACTOR));
+            } else {
+                outOfReach.put(n.getId(), n);
+            }
+        }
+    }
+}
+
---
+++ src/student/ShortestPathFinder.java
@@ -0,0 +1,140 @@
+package student;
+
+import game.Edge;
+import game.EscapeState;
+import game.Node;
+
+import java.util.*;
+
+public class ShortestPathFinder {
+
+    private EscapeState state;
+    private Map<Long, DijVertex> graph;
+    private Map<Long, DijVertex> workingVertices;
+    private Map<Long, DijVertex> labelledVertices;
+    private DijVertex source;
+    private DijVertex destination;
+    private int currentOrder = 1;
+
+    private int distance;
+    private Stack<DijVertex> path;
+
+    public ShortestPathFinder(EscapeState state, Node start, Node end) {
+        this.state = state;
+        rebuildGraph();
+        source = graph.get(start.getId());
+        destination = graph.get(end.getId());
+        calculatePath();
+    }
+
+    private void rebuildGraph() {
+        graph = new HashMap<>();
+        workingVertices = new HashMap<>();
+        labelledVertices = new HashMap<>();
+        Collection<Node> nodes = state.getVertices();
+        for(Node n : nodes) {
+            graph.put(n.getId(), new DijVertex(n));
+        }
+    }
+
+    private void calculatePath() {
+        DijVertex currentVertex = source;
+        label(currentVertex);
+        while(currentVertex.getNode().getId() != destination.getNode().getId()) {
+            List<Long> neighbours = getNeighbours(currentVertex);
+            assignWorkingValues(currentVertex, neighbours);
+            currentVertex = smallestWorkingValue();
+            label(currentVertex);
+        }
+        this.distance = currentVertex.getFinalValue();
+        this.path = traceRoute();
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public Stack<DijVertex> getPath() {
+        return path;
+    }
+
+    private void label(DijVertex v) {
+        if(v.getWorkingValue() == -1) {
+            v.setFinalValue(0);
+        } else {
+            v.setFinalValue(v.getWorkingValue());
+        }
+        v.setOrder(currentOrder);
+        currentOrder++;
+        labelledVertices.put(v.getNode().getId(), v);
+        if(graph.containsKey(v.getNode().getId())) {
+            graph.remove(v.getNode().getId());
+        } else {
+            workingVertices.remove(v.getNode().getId());
+        }
+
+    }
+
+    private List<Long> getNeighbours(DijVertex v) {
+        Set<Edge> edges = v.getNode().getExits();
+        List<Long> neighbours = new ArrayList<>();
+        for(Edge e : edges) {
+            neighbours.add(e.getOther(v.getNode()).getId());
+        }
+        return neighbours;
+    }
+
+    private void assignWorkingValues(DijVertex v, List<Long> neighbours) {
+        for(Long l : neighbours) {
+            DijVertex neighbour;
+            if(labelledVertices.containsKey(l)) {
+                neighbour = labelledVertices.get(l);
+            } else if(workingVertices.containsKey(l)) {
+                neighbour = workingVertices.get(l);
+            } else {
+                neighbour = graph.get(l);
+            }
+            if(neighbour.getOrder() == -1) {
+                neighbour.setWorkingValue(v.getFinalValue() + v.getNode().getEdge(neighbour.getNode()).length());
+                workingVertices.put(l, neighbour);
+                graph.remove(l);
+            }
+
+        }
+    }
+
+    private DijVertex smallestWorkingValue() {
+        List<DijVertex> list = new ArrayList<>(workingVertices.values());
+        list.sort((v1, v2) -> v1.getWorkingValue() - v2.getWorkingValue());
+        return list.get(0);
+    }
+
+    private Stack<DijVertex> traceRoute() {
+        Stack<DijVertex> route = new Stack<>();
+        route.push(destination);
+        DijVertex temp = destination;
+        while(!temp.equals(source)) {
+            List<Long> neighbours = getNeighbours(temp);
+            List<DijVertex> vToStack = new ArrayList<>();
+            for(Long l : neighbours) {
+                if(labelledVertices.containsKey(l)) {
+                    DijVertex newTemp = labelledVertices.get(l);
+                    if(newTemp.getFinalValue() + temp.getNode().getEdge(newTemp.getNode()).length() == temp.getFinalValue()) {
+                        vToStack.add(newTemp);
+                    }
+                }
+            }
+            route.push(vToStack.get(0));
+            temp = vToStack.get(0);
+        }
+        return route;
+    }
+
+    @Override
+    public String toString() {
+        return "From node: " + source.getNode().getId()
+                + "\nTo node: " + destination.getNode().getId()
+                + "\nTime to follow: " + distance
+                + "\n";
+    }
+}
