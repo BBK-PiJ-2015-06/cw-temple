58c4e6812285b4e8f99dc3864c3f46b3808a669d
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-04-10 12:02:30 +0100

    Working solution created for single thread computation. Calculates a random path through the cavern, terminating at the exit. Calculates the amount of gold on route as well as the time taken to traverse, returned in a future.

--- src/student/EscapeTask.java
+++ src/student/EscapeTask.java
@@ -5,8 +5,10 @@ import game.Node;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Random;
 import java.util.Stack;
 import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
 
 public class EscapeTask implements Callable {
 
@@ -15,22 +17,41 @@ public class EscapeTask implements Callable {
     private int goldCollected;
     private int timeElapsed;
     private List<Node> route;
-    private Stack wayFinderRoute;
+    private Stack<Node> wayFinderRoute;
 
-    public EscapeTask(EscapeState state, Node currentNode, int goldCollected, int timeElapsed, List<Node> route, Stack wayFinderRoute) {
+    public EscapeTask(EscapeState state, Node startNode, int goldCollected, int timeElapsed, List<Node> route, Stack<Node> wayFinderRoute) {
         this.state = state;
-        this.currentNode = currentNode;
+        this.currentNode = startNode;
         this.goldCollected = goldCollected;
         this.timeElapsed = timeElapsed;
         this.route = route;
         this.wayFinderRoute =wayFinderRoute;
+        this.wayFinderRoute.push(currentNode);
     }
 
     @Override
     public Object call() throws Exception {
-        Collection<Node> neighbours = currentNode.getNeighbours();
-        System.out.println(neighbours);
+        while(!currentNode.equals(state.getExit())) {
+            route.add(currentNode);
+            goldCollected = goldCollected + currentNode.getTile().getGold();
 
+            Collection<Node> neighbours = currentNode.getNeighbours();
+            List<Node> newNeighbours = neighbours.stream().filter(n -> !route.contains(n)).collect(Collectors.toList());
+
+            Node nextNode;
+            if(!newNeighbours.isEmpty()) {
+                Random random = new Random();
+                int max = newNeighbours.size() - 1;
+                nextNode = newNeighbours.get(random.nextInt(max + 1));
+                timeElapsed = timeElapsed + currentNode.getEdge(newNeighbours.get(0)).length();
+                wayFinderRoute.push(currentNode);
+                currentNode = newNeighbours.get(0);
+            } else {
+                nextNode = wayFinderRoute.pop();
+                timeElapsed = timeElapsed + currentNode.getEdge(nextNode).length();
+            }
+            currentNode = nextNode;
+        }
         return new EscapeTaskResult(goldCollected, timeElapsed, route);
     }
 }
