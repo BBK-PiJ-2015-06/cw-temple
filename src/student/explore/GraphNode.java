308051811e0f371e54821e893b9e07681d21a07a
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-04-11 14:17:48 +0100

    Javadoc comments added to GraphNode class.

--- src/student/explore/GraphNode.java
+++ src/student/explore/GraphNode.java
@@ -3,20 +3,40 @@ package student.explore;
 import game.NodeStatus;
 import java.util.Collection;
 
+/**
+ * This class represents a basic node within the graph used by ExploreRunner.
+ * It holds information about a tile's ID as, well as it's traversable neighbours.
+ * GraphNodes are created during the explore phase as the sprite explores the maze,
+ * building the graph gradually to enable the shortest path algorithm to work sufficiently.
+ */
 public class GraphNode {
 
     private Long id;
     private Collection<NodeStatus> neighbours;
 
+    /**
+     * Creates a GraphNode object
+     * @param id the ID of the NodeStatus object represented by this GraphNode
+     * @param neighbours a collection of NodeStatus objects that are neighbours of the node that the ID
+     *                   corresponds to.
+     */
     public GraphNode(Long id, Collection<NodeStatus> neighbours) {
         this.id = id;
         this.neighbours = neighbours;
     }
 
+    /**
+     * Returns the ID of this GraphNode
+     * @return the ID
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Returns the neighbours of this GraphNode
+     * @return the neighbours
+     */
     public Collection<NodeStatus> getNeighbours() {
         return neighbours;
     }
