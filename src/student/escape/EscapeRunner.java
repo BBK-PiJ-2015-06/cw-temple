4111db1e6ddae28ed1f5f80b3bda3862683bb066
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-04-11 17:39:51 +0100

    Javadoc comments added to EscapeRunner class.

--- src/student/escape/EscapeRunner.java
+++ src/student/escape/EscapeRunner.java
@@ -7,6 +7,15 @@ import student.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * This class represents the algorithm used to enable George to escape from the
+ * maze, while picking up gold that he encounters. This class makes heavy use of
+ * Dijkstra's algorithm to continuously compute the shortest distance to the exit
+ * so that he always makes it out of the maze before time runs out. The decision of
+ * which next Node George wil attempt to reach is calculated based upon a gold and
+ * distance factor (considers the shortest distance to the node and the amount of gold
+ * located there and en route)
+ */
 public class EscapeRunner {
 
     private EscapeState currentState;
@@ -17,6 +26,10 @@ public class EscapeRunner {
     private static final double GOLD_FACTOR = 1;
     private static final double DIST_FACTOR = 1.9;
 
+    /**
+     * Constructs the EscapeRunner object, using an EscapeState.
+     * @param state the current EscapeState
+     */
     public EscapeRunner(EscapeState state) {
         currentState = state;
         goldNodes = currentState.getVertices();
@@ -30,6 +43,10 @@ public class EscapeRunner {
         updateGoldRank();
     }
 
+    /**
+     * The main algorithm used by this EscapeRunner object to perform the escape phase
+     * of the coursework.
+     */
     public void escapeMaze() {
         while(currentState.getCurrentNode().getId() != currentState.getExit().getId()) {
             if(goldRank.size() == 0) {
@@ -44,6 +61,10 @@ public class EscapeRunner {
         }
     }
 
+    /**
+     * Moves the sprite along the path specified.
+     * @param path a path to be followed during the escape phase
+     */
     private void followPath(Stack<DijVertex> path) {
         path.pop();
         while (!path.empty()) {
@@ -55,11 +76,21 @@ public class EscapeRunner {
         }
     }
 
+    /**
+     * Updates the goldNodes field. The updated field will only contain those nodes within
+     * the maze that contain gold and are within reach within the allocated time. A node is deemed
+     * to be within reach if the time taken to reach it plus the time taken to reach the exit is less
+     * than the remaining time limit.
+     */
     private void updateGoldNodes() {
         goldNodes = goldNodes.stream().filter(n ->
                 n.getTile().getGold() > 0 && !outOfReach.containsKey(n.getId())).collect(Collectors.toList());
     }
 
+    /**
+     * Updates the goldRank field based upon the revised goldNodes list. Nodes are ranked in order of priority
+     * based upon the amount of gold on the node as well as the distance to the node from the current location.
+     */
     private void updateGoldRank() {
         goldRank = new PriorityQueueImpl<>();
         for(Node n : goldNodes) {
