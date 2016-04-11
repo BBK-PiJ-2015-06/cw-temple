8cbc36e87505248c2f2370a5f5fe74bb3a7df679
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-04-11 14:11:21 +0100

    Javadoc comments added to ExploreRunner class.

--- src/student/explore/ExploreRunner.java
+++ src/student/explore/ExploreRunner.java
@@ -10,6 +10,14 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.Stack;
 
+/**
+ * This class represents the algorithm used by the sprite to explore the maze. An object is
+ * created for each instance of a game being played and incrementally builds a map as the
+ * sprite explores the maze. This map is then used to enable the sprite to always move to the
+ * tile that is closest to the orb. The algorithm uses a simplified version of Dijkstra's
+ * shortest path finder to enable George to take the shortest route to the node that is closest
+ * to the orb, even if it is not a neighbour of the current tile that he resides upon.
+ */
 public class ExploreRunner {
 
     private ExplorationState currentState;
@@ -18,6 +26,11 @@ public class ExploreRunner {
     private PriorityQueue<NodeStatus> rankedByDistance;
     private Collection<GraphNode> map;
 
+    /**
+     * Creates an ExploreRunner object using an ExplorationState object. This constructor gets
+     * called from the Explorer class in package student.
+     * @param state the initial ExplorationState to be used when exploring the maze
+     */
     public ExploreRunner(ExplorationState state) {
         currentState = state;
         explored = new HashSet<>();
@@ -26,6 +39,12 @@ public class ExploreRunner {
         map = new HashSet<>();
     }
 
+    /**
+     * Method used for finding the orb. This method builds the map with each node (and corresponding neighbours)
+     * explored and updates the explored and not explored sets. Using these collections, a path is generated to
+     * an as yet unexplored node that is closest to the orb. The aforementioned process is repeated until the sprite
+     * resides upon the node where the orb is located.
+     */
     public void findOrb() {
         while(currentState.getDistanceToTarget() != 0) {
 
